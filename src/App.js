d77c6304f39c7d5d6fd23bddaadfd775f363b82a
david <puho999@hotmail.com.tw>
2021-08-04 21:12:22 +0800

    9/12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState, useMemo, memo } from 'react';
+
+function Text({ fruits }) {
+  console.log('render Text');
+
+  useEffect(() => {
+    console.log('something changed!');
+  }, [fruits]);
+
+  return (
+    <>
+      <div>banana:{fruits[0]}</div>
+      <div>orange:{fruits[1]}</div>
+    </>
+  );
+}
+const MemoText = memo(Text)
+
 function App() {
+  console.log('render app');
+  const [apple, setApple] = useState(1);
+
+  let fruits = [];
+  const [banana, setBanana] = useState(2);
+  const [orange, setOrange] = useState(3);
+  fruits = useMemo(() => [banana, orange], [banana, orange]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={() => setApple(apple + 1)}>
+        apple +
+      </button>
+      <button type="button" onClick={() => setBanana(banana + 1)}>
+        banana +
+      </button>
+      <button type="button" onClick={() => setOrange(orange + 1)}>
+        orange +
+      </button>
+      <div>apple: {apple}</div>
+      <MemoText fruits={fruits} />
     </div>
   );
 }
